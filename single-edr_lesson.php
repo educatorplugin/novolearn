203d508f875fe6fcdc19620c151b0e1314c0d3c0
Dmytro <educatorplugin@users.noreply.github.com>
2017-01-03 17:57:33 +0200

    Added lessons navigation

--- single-edr_lesson.php
+++ single-edr_lesson.php
@@ -11,6 +11,15 @@
 						if ( comments_open() || get_comments_number() ) {
 							comments_template();
 						}
+
+						the_post_navigation( array(
+							'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'novolearn' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Next lesson:', 'novolearn' ) . '</span> ' .
+								'<span class="post-title">%title</span>',
+							'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'novolearn' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Previous lesson:', 'novolearn' ) . '</span> ' .
+								'<span class="post-title">%title</span>',
+						) );
 					endwhile;
 				?>
 			</div>
